f6e3f3058be84094ac50d1ebc3f1b278d70e20e2
Clarence Subia <clarence.subia@netsuite.com>
2022-09-21 22:37:37 +0800

    First commit

---
+++ arigator.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+from googletrans import Translator
+from prettytable import PrettyTable
+import argparse
+import colorama
+
+parser = argparse.ArgumentParser(description="Japanese translator.")
+parser.add_argument("-t", "--target", required=False, help="Target File containing lists of phrases", metavar="")
+args = parser.parse_args()
+target = args.target
+
+word_table = PrettyTable(["English", "Japanese", "Pronunciation"])
+
+
+def main():
+    if target:
+        with open(target, "r") as file:
+            phrases = file.readlines()
+            prog_bar(0, len(phrases))
+            for index, line in enumerate(phrases):
+                translator(line)
+                prog_bar(index + 1, len(phrases))
+            print(colorama.Fore.RESET)
+    else:
+        word = input("Enter word/phrase: ")
+        translator(word)
+    print("\nResults:")
+    print(word_table)
+    
+
+def translator(target):
+    translator = Translator()
+    translations = translator.translate(target, dest="ja")
+    word_table.add_row([translations.origin, translations.text, translations.pronunciation])
+    
+
+
+def prog_bar(progress, total, color=colorama.Fore.YELLOW):
+    percent = 100 * (progress / float(total))
+    percent = int(percent)
+    bar = "█" * percent + "-" * (100 - percent)
+    print(color + f"\r|{bar}| {percent:.2f}%", end="\r")
+    if progress == total:
+        print(colorama.Fore.GREEN + f"\r|{bar}| {percent:.2f}%", end="\r")
+
+
+    
+
+
+if __name__ == "__main__": main()
