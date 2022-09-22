b001757b697f4429fb92d7bc5d6197764d65493c
Clarence Subia <clarence.subia@netsuite.com>
2022-09-22 12:26:40 +0800

    Added pron

--- arigator.py
+++ arigator.py
@@ -4,6 +4,7 @@ from googletrans import Translator
 from prettytable import PrettyTable
 import argparse
 import colorama
+import sys
 
 
 parser = argparse.ArgumentParser(description="Japanese translator.")
@@ -12,23 +13,28 @@ args = parser.parse_args()
 target = args.target
 
 word_table = PrettyTable(["Source", "Translation", "Pronunciation"])
+word_table.padding_width = 1
 
 
 def main():
-    if target:
-        with open(target, "r") as file:
-            phrases = file.readlines()
-            prog_bar(0, len(phrases))
-            for index, line in enumerate(phrases):
-                translator(line)
-                prog_bar(index + 1, len(phrases))
-            print(colorama.Fore.RESET)
-            print(word_table)
-    else:
-        while True:
-            word = input("Enter word/phrase: ")
-            translator(word)
-            print(word_table)
+    try:
+        if target:
+            with open(target, "r") as file:
+                phrases = file.readlines()
+                prog_bar(0, len(phrases))
+                for index, line in enumerate(phrases):
+                    translator(line)
+                    prog_bar(index + 1, len(phrases))
+                print(colorama.Fore.RESET)
+                print(word_table)
+        else:
+            while True:
+                word = input("Enter word/phrase: ")
+                translator(word)
+                print(word_table)
+    except KeyboardInterrupt:
+        print("Quitting...")
+        sys.exit()
     
     
 
@@ -38,8 +44,11 @@ def translator(target):
 
     if source.lang == "ja":
         translations = translator.translate(target, dest="en")
-        pronunciation = translator.translate(translations.text, dest="ja")
-        word_table.add_row([translations.origin, translations.text, pronunciation.pronunciation])
+        pron = translations.pronunciation
+        if not translations.pronunciation:
+            pronunciation = translator.translate(translations.text, dest="ja")
+            pron = pronunciation.pronunciation
+        word_table.add_row([translations.origin, translations.text, pron])
     elif source.lang == "en":
         translations = translator.translate(target, dest="ja")
         word_table.add_row([translations.origin, translations.text, translations.pronunciation])
